4cd5aa3ed41e425f0dda26e7e6402cf169407b1e
kiranvadhel <kiranvadhel19@gmail.com>
2026-01-28 23:14:29 +0530

    Added components, pages, templates and assets

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Banner from '../components/home/Banner'
+import Hero from '../components/hero'
+
+
+const Home = () => {
+  return (
+    <div>
+      <Banner/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default Home
---
+++ client/src/pages/ResumeBuilder.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ResumeBuilder = () => {
+  return (
+    <div><h1>ResumeBuilder page</h1></div>
+  )
+}
+
+export default ResumeBuilder
